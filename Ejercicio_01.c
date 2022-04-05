5e392fa9bf92f5151670c297acad5e8dfe44d190
AgusB1101 <agustinbernal69@gmail.com>
2022-04-05 00:07:19 -0300

    Done: Added function mail_split() and finished exercise 1

--- Ejercicio_01.c
+++ Ejercicio_01.c
@@ -5,6 +5,8 @@ int length(const char *);
 char *copy(char *);
 char *string_concat(const char *, const char *);
 void string_concat_dynamic(const char *, const char *, char **);
+char *char_to_string(char);
+void mail_split(const char *, char **, char **);
 
 int main()
 {
@@ -15,6 +17,13 @@ int main()
 
   string_concat_dynamic("Hola ", result, &saludo);
   printf("%s\n", saludo);
+
+  char *mail = "ritchie@ansic.com.ar";
+  char *user;
+  char *dom;
+  mail_split(mail, &user, &dom);
+
+  printf("User: %s\tDom:%s\n", user, dom);
 }
 
 int length(const char *str)
@@ -52,4 +61,38 @@ char *string_concat(const char *str1, const char *str2)
 void string_concat_dynamic(const char *str1, const char *str2, char **buffer)
 {
   *buffer = copy(string_concat(str1, str2));
+}
+
+char *char_to_string(char c)
+{
+  char *tmp = malloc(sizeof(char) * 2);
+  sprintf(tmp, "%c", c);
+
+  return tmp;
+}
+
+void mail_split(const char *mail, char **user, char **dom)
+{
+  char **temp = malloc(sizeof(char *) * 2);
+  temp[0] = "";
+  temp[1] = "";
+
+  int i = 0;
+
+  while (mail[i] != '@')
+  {
+    string_concat_dynamic(temp[0], char_to_string(mail[i]), &temp[0]);
+    i++;
+  }
+
+  i++;
+
+  while (mail[i] != '\0')
+  {
+    string_concat_dynamic(temp[1], char_to_string(mail[i]), &temp[1]);
+    i++;
+  }
+
+  *user = copy(temp[0]);
+  *dom = copy(temp[1]);
 }
